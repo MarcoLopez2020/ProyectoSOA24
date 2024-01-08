c011dd0d9deb53798379a5379af3989888655b74
MarcoLopez2020 <74079451+MarcoLopez2020@users.noreply.github.com>
2024-01-08 10:56:05 -0500

    Update ApiEstudiante.java

--- src/main/java/uta/edu/estudiante/controllers/ApiEstudiante.java
+++ src/main/java/uta/edu/estudiante/controllers/ApiEstudiante.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 import java.util.ArrayList;
 
 @RestController
-@RequestMapping("rest")
+@RequestMapping("/api") 
 public class ApiEstudiante {
     @Autowired
     private EstudianteRepository estudianteRepository;
